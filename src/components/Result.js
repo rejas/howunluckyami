d937fdf58f65bee8473117ba0757785574b167c6
rejas <github@veeck.de>
2017-11-09 08:43:40 +0100

    Cleanups

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
+import './Result.css';
+
 function Result(props) {
     const result = Math.floor((1 - ( Math.pow(1 - props.item.rate, props.tries))) * 10000) / 100,
           link = "http://www.wowhead.com/item=" + props.item.id;
 
     return (
         <Jumbotron>
-            <p className="h3 col-sm-12 text-center">The chance of dropping the item at least once in you runs is:<br/>
-                <span className="h2 js-result text-success">{result} %</span>
-            </p>
-            <a target="_blank" href={link}>link to wowhead page</a>
+            <h3 className="h3 col-sm-12 text-center">
+                The chance of dropping <a target="_blank" href={link}>{props.item.name}</a> at least once in your runs is:
+            </h3>
+            <h2 className="h2 js-result text-success">
+                {result} %
+            </h2>
         </Jumbotron>
     );
 }

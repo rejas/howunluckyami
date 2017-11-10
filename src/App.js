d25c0bf2b1ea4c22ca3d426233f8761ee3fd9ae6
rejas <github@veeck.de>
2017-11-11 00:20:41 +0100

    Fix routing, no state propagation yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import {
+    Link,
     Route,
     NavLink,
     HashRouter
@@ -11,10 +12,12 @@ import Logo from './components/Logo.js';
 import Category from './components/Category.js';
 import Result from './components/Result.js';
 
+import Home from './home.js';
+import Mounts from './wow/mounts.js';
 import Pets from './wow/pets.js';
 import Toys from './wow/toys.js';
 
-import { ButtonGroup, ControlLabel, FormControl, FormGroup, Grid, Jumbotron, Row } from 'react-bootstrap';
+import { Button, ButtonGroup, ControlLabel, FormControl, FormGroup, Grid, Jumbotron, Row } from 'react-bootstrap';
 
 import './App.css';
 
@@ -68,24 +71,20 @@ class App extends Component {
             <HashRouter>
                 <div className="hulai">
                     <Jumbotron>
-                        <Logo />
-                        <h1>How unlucky am I?</h1>
+                        <Logo/>
+                        <Link to="/"><h1>How unlucky am I?</h1></Link>
                     </Jumbotron>
 
                     <Grid>
-                        <li><NavLink to="/">Mounts</NavLink></li>
-                        <li><NavLink to="/pets">Pets</NavLink></li>
-                        <li><NavLink to="/">Toys</NavLink></li>
-
-                        <ButtonGroup onClick={this.handleTypeChange}>
-                            <Category class="active" type={'mounts'} />
-                            <Category type={'pets'} />
-                            <Category type={'toys'} />
+                        <ButtonGroup>
+                            <Link to="/mounts"><Button>Mounts</Button></Link>
+                            <Link to="/pets"><Button>Pets</Button></Link>
+                            <Link to="/"><Button>Toys</Button></Link>
                         </ButtonGroup>
 
-                        <div class="content">
-                            <Route exact path="/" component={Toys}/>
-                            <Route path="/mounts" component={Toys}/>
+                        <div className="content">
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/mounts" component={Mounts}/>
                             <Route path="/pets" component={Pets}/>
                             <Route path="/toys" component={Toys}/>
                         </div>
---
+++ src/wow/mounts.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import { ControlLabel, FormControl, FormGroup, Row } from 'react-bootstrap';
+
+class Mounts extends React.Component {
+    render() {
+        return (
+            <Row>
+                <h1>Mounts</h1>
+            </Row>
+        );
+    }
+}
+
+export default Mounts;
